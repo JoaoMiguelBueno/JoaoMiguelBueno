f0582054c252543687b3b4890dcbfd9b4ee8dedf
JoaoMiguelBueno <buenomiguel016@gmail.com>
2025-05-09 13:28:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function navegarCards(direcao) {
+  const faixa = document.getElementById("carrossel-cards");
+  faixa.scrollBy({
+    left: 320 * direcao,
+    behavior: "smooth"
+  });
+}
 
+function navegarVideos(direcao) {
+  const faixa = document.getElementById("carrossel-videos");
+  faixa.scrollBy({
+    left: 320 * direcao,
+    behavior: "smooth"
+  });
+}
